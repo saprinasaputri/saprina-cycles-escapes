29eddff6abaf6849604989a54760f097c6b881f7
Saprina Saputri <114340488+saprinasaputri@users.noreply.github.com>
2024-03-05 00:36:36 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
+
 # Set style seaborn
 sns.set(style='dark')
 # Menyiapkan daily_rent_df
